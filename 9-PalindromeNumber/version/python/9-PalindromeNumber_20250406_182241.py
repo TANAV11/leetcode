3e8ef2e00dee4887c9563f9e5f687958f1e2323b
TANAV11 <97797257+TANAV11@users.noreply.github.com>
2025-04-06 18:22:42 +0530

    Create file

---
+++ 9-PalindromeNumber/version/python/9-PalindromeNumber_20250406_182241.py
@@ -0,0 +1,8 @@
+# Last updated: 4/6/2025, 6:22:41 PM
+class Solution(object):
+    def isPalindrome(self, x):
+        selfx= str(x)
+        return(selfx == selfx[::-1])
+
+                
+        
